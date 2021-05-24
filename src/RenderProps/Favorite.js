d34aad8102e3706979b330c3a6028dd6c4afdd57
Jake Joseph <jakejosephcs@gmail.com>
2021-05-24 15:53:37 -0400

    Update to include toggler component

--- src/RenderProps/Favorite.js
+++ src/RenderProps/Favorite.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Toggler from './Toggler';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
@@ -6,15 +7,28 @@ import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
 function Favorite() {
   return (
-    <div>
-      <FontAwesomeIcon className='fa-2x' icon={faHeart} />
-
-      <FontAwesomeIcon
-        style={{ color: 'red' }}
-        className='fa-2x'
-        icon={faHeartbeat}
+    <>
+      <Toggler
+        render={(on, toggle) => (
+          <div>
+            {on ? (
+              <FontAwesomeIcon
+                className='fa-2x'
+                icon={faHeart}
+                onClick={toggle}
+              />
+            ) : (
+              <FontAwesomeIcon
+                style={{ color: 'red' }}
+                className='fa-2x'
+                icon={faHeartbeat}
+                onClick={toggle}
+              />
+            )}
+          </div>
+        )}
       />
-    </div>
+    </>
   );
 }
 
