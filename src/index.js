9f0e120a9332fb0d1aa8d3fc93b6809bd92c56f9
Jake Joseph <jakejosephcs@gmail.com>
2021-05-25 16:56:54 -0400

    Update to use Context folder and implement context provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './RenderProps/App';
+import App from './Context/App';
+import { ThemeContextProdivder } from './Context/themeContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeContextProdivder>
+    <App />
+  </ThemeContextProdivder>,
+  document.getElementById('root')
+);
