6a77997f3bd94daafddd239a520874c15afbcd8c
Jake Joseph <jakejosephcs@gmail.com>
2021-05-25 16:54:44 -0400

    Update header to implement useContext hook for theme

--- src/Context/Header.js
+++ src/Context/Header.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from './themeContext';
 
 export default function Header() {
+  const { theme } = useContext(ThemeContext);
+
   const themeStyles = {
-    backgroundColor: 'brown',
+    backgroundColor: theme === 'dark' ? 'black' : 'white',
+    color: theme === 'dark' ? 'white' : 'black',
+    padding: '0.5em 2em',
+    marginBottom: '1em',
+    border: '1px solid black',
   };
 
   return (
